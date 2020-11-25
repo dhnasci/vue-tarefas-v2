b3cc23e8d943b3e185008ab1150188c098370550
Dirceu Henrique <dhnasci@hotmail.com>
2020-11-25 17:49:38 -0400

    Implementando demais mutations do CRUD

--- src/resources/tarefas/_store/mutations.js
+++ src/resources/tarefas/_store/mutations.js
@@ -1,9 +1,25 @@
 import {
-     LISTAR_TAREFAS 
+     LISTAR_TAREFAS,
+     EDITAR_TAREFA,
+     CRIAR_TAREFA,
+     DELETAR_TAREFA
 } from './mutation-types'
 
 export default {
     // para ser usado com operações sincronas
+    [CRIAR_TAREFA]: (state, { tarefa }) => {
+        state.tarefas.push(tarefa)
+    },
+    [EDITAR_TAREFA]: (state, { tarefa }) => {
+        const indice = state.tarefas.findIndex(t => t.id === tarefa.id)
+        state.tarefas.splice(indice, 1, tarefa)
+        
+    },
+    [DELETAR_TAREFA]: (state, { tarefa }) => {
+        const indice = state.tarefas.findIndex(t => t.id === tarefa.id)
+        state.tarefas.splice(indice, 1)
+        
+    },
     [LISTAR_TAREFAS]: (state, { tarefas }) => {
         state.tarefas = tarefas
     }
